b304766c5bfd7f18aed11f2a61601507d74bd6b6
Evgeny Novgorodov <blissfulman01@gmail.com>
2020-11-14 20:11:40 +0500

    Empty project

---
+++ Weather/Additional Files/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by User on 14.11.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
--- Weather/View Controllers/MainViewController.swift
+++ Weather/View Controllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Weather
 //
 //  Created by User on 14.11.2020.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
